b603b94dfd0c99b93c69f9407cd6147b4e930f09
Fathima Shajahan <shajahanfathima079@gmail.com>
2026-04-16 15:08:37 +0530

    fix merge conflict

--- src/services/productapi.js
+++ src/services/productapi.js
@@ -1,5 +1,4 @@
-<<<<<<< HEAD
-import api from "../api/api"
+import api from "../api/api";
 
 export const getAllProducts = async () => {
   const res = await api.get("/api/products");
@@ -13,21 +12,3 @@ export const addProducts = (data) => api.post("/api/products", data);
 export const updateProduct = (id, data) => api.put(`/api/products/${id}`, data);
 
 export const deleteProduct = (id) => api.delete(`/api/products/${id}`);
-
-=======
-import api from "../api/api"
-
-export const getAllProducts = async () => {
-  const res = await api.get("/api/products");
-  return res.data;
-};
-
-export const getProductById = (id) => api.get(`/api/products/${id}`);
-
-export const addProducts = (data) => api.post("/api/products", data);
-
-export const updateProduct = (id, data) => api.put(`/api/products/${id}`, data);
-
-export const deleteProduct = (id) => api.delete(`/api/products/${id}`);
-
->>>>>>> 2ec2d8f07764667d1cb3efdf808e30e6e1ae9d32
