f959e0e72e74963afdb60fc7f9b79843e78a9ef5
Fathima Shajahan <shajahanfathima079@gmail.com>
2026-04-16 14:44:51 +0530

    fixed api config

--- src/api/api.js
+++ src/api/api.js
@@ -1,16 +1,7 @@
-<<<<<<< HEAD
 import axios from "axios";
 
 const api = axios.create({
-    baseURL: import.meta.env.VITE_API_URL,
+  baseURL: import.meta.env.VITE_API_URL,
 });
 
-=======
-import axios from "axios";
-
-const api = axios.create({
-    baseURL: import.meta.env.VITE_API_URL,
-});
-
->>>>>>> 2ec2d8f07764667d1cb3efdf808e30e6e1ae9d32
 export default api;
